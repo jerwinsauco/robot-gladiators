aadd5c68f0f90db6dd193f5b2707644765da3df3
Jerwin <jerwinsauco@gmail.com>
2022-07-26 16:10:03 -0400

    shop function

--- game.js
+++ game.js
@@ -130,6 +130,13 @@ var endGame = function() {
   }
 };
 
+// go to shop between battles function
+var shop = function() {
+  // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+  );
+
   // use switch case to carry out action
   switch (shopOptionPrompt) {
     case 'REFILL':
